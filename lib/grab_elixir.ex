0751e9283f213cded13122bd11d6b1684b526fb7
denispeplin <denis.peplin@gmail.com>
2019-11-14 15:38:55 +0400

    Grab also message bodies from Elixir forum page.

--- lib/grab_elixir.ex
+++ lib/grab_elixir.ex
@@ -3,11 +3,38 @@ defmodule GrabElixir do
 
   def run do
     with {:ok, response} <- HTTPoison.get(@url) do
-      Floki.find(response.body, "span a")
-      |> Enum.map(fn {"a", [{"itemprop", "url"}, {"href", link}],
-                      [{"span", [{"itemprop", "name"}], [username]}]} ->
-        {link, username}
-      end)
+      messages =
+        response.body
+        |> Floki.find("div.topic-body")
+        |> Enum.map(fn body ->
+          post = Floki.raw_html(body)
+
+          [
+            {"a", [{"itemprop", "url"}, {"href", link}],
+             [{"span", [{"itemprop", "name"}], [username]}]}
+          ] = Floki.find(post, "span.creator a")
+
+          post_body =
+            post
+            |> Floki.find("div.post")
+            |> Floki.raw_html()
+
+          {username, link, post_body}
+        end)
+
+      message_bodies =
+        Enum.reduce(messages, %{}, fn {username, _link, post_body}, acc ->
+          # note that bodies will be in reverse order as result
+          Map.put(acc, username, [post_body | Map.get(acc, username) || []])
+        end)
+
+      user_links =
+        Enum.map(messages, fn {username, link, _post_body} ->
+          {username, link}
+        end)
+        |> Enum.uniq()
+
+      {user_links, message_bodies}
     end
   end
 end
